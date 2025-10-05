37e883b0c7fcd5915a0dc770e824823eafa258e9
OkoliDaniel <okolidaniel57@gmail.com>
2025-10-05 05:10:17 +0100

    restore telegram webhook mode

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 	"time"
 
+	"net/http"
 	_ "net/http/pprof"
 
 	"github.com/Builciber/blockbot/internal/database"
@@ -128,12 +129,12 @@ func main() {
 	mux := chi.NewRouter()
 	mux.Post("/webhooks/telegram", b.WebhookHandler())
 
-	b.Start(ctx)
+	go b.StartWebhook(ctx)
 
-	/*server := http.Server{
+	server := http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
 	log.Println("Started server on localhost at port 8080")
-	server.ListenAndServe()*/
+	server.ListenAndServe()
 }
