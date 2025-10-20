78d9f0bc0d5d444bcdcf120dae9e1f1fffa7006c
OkoliDaniel <okolidaniel57@gmail.com>
2025-10-20 23:47:14 +0100

    escape markdown for token names and symbols

--- handlerSellX.go
+++ handlerSellX.go
@@ -111,7 +111,7 @@ func (cfg *apiConfig) handlerProcessSellX(ctx context.Context, b *bot.Bot, msg *
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID:    msg.Chat.ID,
 		ParseMode: models.ParseModeMarkdown,
-		Text:      fmt.Sprintf("Sale successful: Sold *%v %s* for *%v MON*\n[View on the explorer](https://testnet.monadexplorer.com/tx/%s)", strings.Replace(displayDecimal(saleResult.SoldAmount, 3), ".", "\\.", 1), token.Symbol, strings.Replace(displayDecimal(saleResult.ReceivedMon, 3), ".", "\\.", 1), saleResult.TxHash),
+		Text:      fmt.Sprintf("Sale successful: Sold *%v %s* for *%v MON*\n[View on the explorer](https://testnet.monadexplorer.com/tx/%s)", strings.Replace(displayDecimal(saleResult.SoldAmount, 3), ".", "\\.", 1), escapeMarkdown(token.Symbol), strings.Replace(displayDecimal(saleResult.ReceivedMon, 3), ".", "\\.", 1), saleResult.TxHash),
 	})
 	b.DeleteMessages(ctx, &bot.DeleteMessagesParams{
 		ChatID:     msg.Chat.ID,
